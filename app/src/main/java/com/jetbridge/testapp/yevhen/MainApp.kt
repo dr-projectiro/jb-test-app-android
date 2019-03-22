4f4bc22be86cfb4472ead6cac84c93c87e13779d
yevhen <nechiporenko.evgeniy@gmail.com>
2019-03-22 15:35:09 +0200

    Change url to cloud backend instance

--- app/src/main/java/com/jetbridge/testapp/yevhen/MainApp.kt
+++ app/src/main/java/com/jetbridge/testapp/yevhen/MainApp.kt
@@ -2,7 +2,7 @@ package com.jetbridge.testapp.yevhen
 
 import android.app.Application
 
-const val API_BASE_URL = "http://192.168.1.127:15050"
+const val API_BASE_URL = "http://185.236.77.69:15050"
 
 class MainApp : Application() {
 
